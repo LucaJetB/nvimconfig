9b6b667ae3ac093b0ea3aaa0cac72aade833d464
LucaJetB <ljburlacu@gmail.com>
2026-07-02 14:20:42 -0400

    lua file update

--- init.lua
+++ init.lua
@@ -21,6 +21,7 @@ vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.undofile = true
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.updatetime = 300
 
 -- Leader key
 vim.g.mapleader = " "
@@ -95,14 +96,16 @@ require('lazy').setup({
       map('n', '<leader>fg', require('telescope.builtin').live_grep)
       map('n', '<leader>fb', require('telescope.builtin').buffers)
       map('n', '<leader>fh', require('telescope.builtin').help_tags)
+      map('n', '<leaderfd>', require('telescope.builtin').diagnostics)
     end,
   },
   {
     'nvim-treesitter/nvim-treesitter',
+    version = 'v0.9.3',
     build = ':TSUpdate',
     config = function()
-      require('nvim-treesitter').setup({
-        ensure_installed = {'lua', 'python', 'rust', 'toml', 'json', 'markdown' },
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { 'lua', 'python', 'rust', 'toml', 'json', 'markdown', 'css', 'javascript', 'html' },
         auto_install = true,
         highlight = {
           enable = true,
@@ -124,7 +127,7 @@ require('lazy').setup({
     dependencies = { 'williamboman/mason.nvim' },
     config = function()
       require('mason-lspconfig').setup({
-        ensure_installed = { 'pyright', 'rust_analyzer' },
+        ensure_installed = { 'pyright', 'rust_analyzer', 'cssls', 'ts_ls', 'html' },
         automatic_installation = true,
       })
     end,
@@ -135,10 +138,24 @@ require('lazy').setup({
     config = function()
       vim.lsp.config('pyright', {})
       vim.lsp.config('rust_analyzer', {})
+      vim.lsp.config('cssls', {})
+      vim.lsp.config('ts_ls', {})
+      vim.lsp.config('html', {})
 
       -- Keymaps that only apply when LSP active
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(event)
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client.supports_method('textDocument/documentHighlight') then
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = event.buf,
+              callback = vim.lsp.buf.document_highlight,
+            })
+            vim.api.nvim_create_autocmd('CursorMoved', {
+              buffer = event.buf,
+              callback = vim.lsp.buf.clear_references,
+            })
+          end
           local map = vim.keymap.set
           map('n', 'gd', vim.lsp.buf.definition)
           map('n', 'K', vim.lsp.buf.hover)
@@ -173,8 +190,10 @@ require('lazy').setup({
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
           ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible then
-              cmp.select_next_item()
+            if cmp.visible() then
+              cmp.confirm({ select = true })
+            elseif luasnip.expand_or_jumpable() then 
+              luasnip.expand_or_jump()
             else
               fallback()
             end
@@ -203,4 +222,9 @@ vim.api.nvim_set_hl(0, 'Keyword', { italic = false})
 vim.api.nvim_set_hl(0, '@keyword', { italic = false})
 vim.api.nvim_set_hl(0, '@keyword.function', { fg = '#f9ada0', italic = false })
 vim.api.nvim_set_hl(0, '@function.builtin', { italic = false})
- 
+
+
+-- Specific highlight
+vim.api.nvim_set_hl(0, 'Keyword', {fg = '#f9ada0', italic  = false })
+vim.api.nvim_set_hl(0, '@keyword.return.python', {fg = '#d2696c', italic = false })
+
